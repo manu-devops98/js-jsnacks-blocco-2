eb2a9d2bfc33e5fb6bfe6d5164b18356eba4489c
manu-devops98 <manuelcappello7@gmail.com>
2021-11-30 12:24:02 +0100

    add esercizio jsnack-2

--- jsnack-2/js/script.js
+++ jsnack-2/js/script.js
@@ -1,1 +1,20 @@
 /* Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari */
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+let sum = 0;
+
+function isEven(num) {
+    if (num % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+for (let i = 0; i < numbers.length; i++) {
+    if (!(isEven(numbers[i]))) {
+        sum += numbers[i];
+    }
+}
+console.log(sum); 
+
